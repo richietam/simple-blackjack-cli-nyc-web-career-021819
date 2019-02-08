fe753a8aa7e700e71d0277f02858015f19550a59
richardtam.ny <richardtam.ny@gmail.com>
2019-02-08 19:09:32 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -31,12 +31,20 @@ def initial_round
   return card_total
 end
 
-def hit?
-  # code hit? here
+def hit?(card_total)
+  prompt_user
+  user_input = get_user_input
+  if user_input == "s"
+    card_total += deal_card
+  elsif  user_input == "h"
+    return card_total
+  else
+    invalid_command
+  end
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "Please enter a valid command"
 end
 
 #####################################################
