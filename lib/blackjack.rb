692da7c3d21a5466eee13ab04da480a02c34b1b1
richardtam.ny <richardtam.ny@gmail.com>
2019-02-08 19:15:02 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -57,8 +57,8 @@ def runner
   welcome
   card total = initial_round
   until card_total > 21
-    hit?(card_total)
+    card total = hit?(card_total)
     display_card_total(card_total)
   end
-  end_game
+  end_game(card_total)
 end
